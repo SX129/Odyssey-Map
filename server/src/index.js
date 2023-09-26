f3e90213704e529e365bb81de198219559870921
Henry Nguyen <nguyenhenry682@gmail.com>
2023-09-26 13:57:01 -0500

    Added middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
+//Middlewares
+//Logs HTTP requests
+const morgan = require('morgan');
+
+//Removes and adds response headers for security
+const helmet = require('helmet');
+
+//Adds cross origin resource sharing header
+const cors = require('cors');
+
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 const port = process.env.PORT || 1337;
 
