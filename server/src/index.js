4947d2c52c0fd326091ecbe51a53169322cff482
Henry Nguyen <nguyenhenry682@gmail.com>
2023-09-26 14:07:18 -0500

    Created error handlers for middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -10,12 +10,23 @@ const helmet = require('helmet');
 //Adds cross origin resource sharing header
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World!"
+    });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 
