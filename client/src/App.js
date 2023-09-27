fbd353e077b440725c807b5128afcecddd3e77ee
Henry Nguyen <nguyenhenry682@gmail.com>
2023-09-26 21:04:09 -0500

    Implemented markers for log entries

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
+import Map, {Marker} from 'react-map-gl';
+import { listLogEntries } from './API';
 
-function App() {
-  return (
-    <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      initialViewState={{
-        longitude: -95.6,
-        latitude: 37.6,
-        zoom: 3.5
-      }}
-      style={{width: '100vw', height: '100vh'}}
-      mapStyle="mapbox://styles/syphe/cln10u41p066s01p78zrs75ik"
-    />
-  );
+const App = () => {
+
+  console.log(listLogEntries);
+
+  return <Map
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    initialViewState={{
+      longitude: -73,
+      latitude: 40,
+      zoom: 3.5,
+    }}
+    style={{width: '100vw', height: 400}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  >
+    <Marker longitude={-73} latitude={40}>
+      <img src="./pin.png" />
+    </Marker>
+  </Map>
 }
 
 export default App;
